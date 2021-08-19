c9148109a3cc2a3df1c3c3b9a99fc5b69e58d88e
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-08-19 17:40:03 +0300

    Added Validation method for Rectangle class
    Some refactors done in Perimeter and square methods.

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ RectangleHelper contains
 1. Rectangle class to calculate perimeter and square of some rectangle.
 
 testaplication demonstrate work of these classes.
+
+# .NET CLI Scripts:
+1. dotnet new classlib
+2. dotnet new classlib
+3. dotnet new console
+4. dotnet new sln 
+  1. dotnet sln add ArrayHelper/Arrayhelper.csproj
+  2. dotnet sln add RectangleHelper/RectangleHelper.csproj
+  3. dotnet sln add testapplication/testapplication.csproj
+  4. dotnet add testapplication/tespapplication.csproj reference Arrayhelper/ArrayHelper.csproj
+  5. dotnet add testapplication/tespapplication.csproj reference Rectanglehelper/RectangleHelper.csproj
+5. dotnet build
+6. dotnet run
+7. dotnet publish
+
+P.s (for creating folders I was using mkdir <folderName>)
--- RectangleHelper/Rectangle.cs
+++ RectangleHelper/Rectangle.cs
@@ -4,22 +4,36 @@ namespace RectangleHelper
 {
     public static class Rectangle
     {
+        private static bool ValidationOfRectangleSides(double a, double b)
+        {
+            if (a <= 0) 
+            {
+                throw new ArgumentOutOfRangeException($"Lenght of side a: {a}  can not be less than 0 or equal");
+            }
+            if (b <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"Lenght of side b: {b}  can not be less than 0 or equal");
+            }
+            return true;      
+        }
         public static double Perimeter(double a, double b)
         {
-            if (a <= 0 || b <= 0)
+            if (ValidationOfRectangleSides(a, b))
             {
-                throw new ArgumentOutOfRangeException("Lenght of side can not be less than 0 or equal");
-            }
-            return (2 * a) + (2 * b);
+                return (2 * a) + (2 * b);
+            } 
+            return -1d;
+            
         }
 
         public static double Square(double a, double b)
         {
-            if (a <= 0 || b <= 0)
+            if (ValidationOfRectangleSides(a, b))
             {
-                throw new ArgumentOutOfRangeException("Lenght of side can not be less than 0 or equal");
+                return a * b;
             }
-            return a * b;
+            return -1d;       
         }
+       
     }
 }
