e1b969ce49c1d096ff209443dd496a624c5b185c
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-08-19 19:12:03 +0300

    Refactor Folders positions

---
+++ M02-Creating-Types/Students/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] subjects = {"Math", "Physics", "Biology", "Chemistry", "PE", "English"};
+
+            var student1 = new Student("Egor.Kanetskin@epam.com");
+            var student2 = new Student("Ivan.Vasiliev@epam.com");
+            var student3 = new Student("Irina.Voznesenkaya@epam.com");
+
+            var student4 = new Student("Evgenii", "Venskii");
+            var student5 = new Student("Lena", "Morskaya");
+            var student6 = new Student("Dima", "Kilnov");
+
+            Console.WriteLine("234635");
+        }
+    }
+}
---
+++ M02-Creating-Types/Students/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+public class Student
+{
+	private string FullName { get; set; }
+
+	private string Email { get; set; }
+
+	public Student(string email)
+    {
+		Email = email;
+		FullName = ParseEmailToFullName(email);
+    }
+
+	public Student(string name, string surname)
+    {
+        FullName = name + " " + surname;
+        Email = GetEmailFromFullName(name, surname);
+    }
+
+	private string ParseEmailToFullName(string email)
+    {
+       StringBuilder fullname = new StringBuilder();
+       for (int i = 0; i < email.Length; i++)
+       {
+            if (email[i].Equals('@')) break;
+            if (email[i].Equals('.'))
+            {
+                fullname.Append(' ');
+            }
+            fullname.Append(email[i]);           
+       }
+       return fullname.ToString();
+      
+    }
+
+	private string GetEmailFromFullName(string name, string surname)
+    {
+        return name + '.' + surname + "@epam.com";
+    }
+
+
+}
