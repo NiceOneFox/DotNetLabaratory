bdb19d64fcd5589ef6d11f49f4b57227275e4523
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-08-19 17:52:03 +0300

    Refactor BubbleSortMethod
    Add parameter to sort asc/desc order.
    Removed unnecessary method.

--- ArrayHelper/BubbleSort.cs
+++ ArrayHelper/BubbleSort.cs
@@ -4,40 +4,29 @@ namespace ArrayHelper
 {
     public static class BubbleSort
     {
-        public static void BubbleSortAscending<T>(T[] array) where T : IComparable<T>
+        /// <param name="array">Input array</param>
+        /// <param name="order">Flag to choose in which order to sort array (ASC/DESC)</param>
+        public static void BubbleSortAscending<T>(T[] array, bool order) where T : IComparable<T>
         {
             T temp;
             for (int i = 0; i < array.Length; i++)
             {
                 for (int j = i + 1; j < array.Length; j++)
                 {
-                    if(array[i].CompareTo(array[j]) > 0)
+                    if (array[i].CompareTo(array[j]) > 0 && order) //ascending order if order true
                     {
                         temp = array[i];
                         array[i] = array[j];
                         array[j] = temp;
-                    }                   
+                    } else if (array[i].CompareTo(array[j]) < 0 && !order)  //descending order if order false
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }                
                 }            
             }
             
-        }
-
-        public static void BubbleSortDescending<T>(T[] array) where T : IComparable<T>
-        {
-            T temp;
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i].CompareTo(array[j]) < 0)
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-
         }
     }
 }
