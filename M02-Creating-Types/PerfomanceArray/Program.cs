6c33aba3a29593e6f49911555529176fc4068b13
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-08-19 22:52:35 +0300

    Migrate from .NET 5 to .NET framework 4.8

---
+++ M02-Creating-Types/PerfomanceArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PerfomanceArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = Process.GetCurrentProcess().PrivateMemorySize64;
+
+            Console.WriteLine(a);
+
+            //Random random = new Random();
+
+            //C[] classes = new C[100000];
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    classes[i].i = random.Next();
+            //}
+
+            //S[] structs = new S[100000];
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    structs[i].i = random.Next();
+
+            }
+    }
+}
---
+++ M02-Creating-Types/PerfomanceArray/S.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfomance
+{
+    struct S
+    {
+        public int i { get; set; }
+    }
+}
