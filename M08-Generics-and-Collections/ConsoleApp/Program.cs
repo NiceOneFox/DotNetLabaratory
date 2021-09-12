9e538885ea4f9917f46eecd0d05c9afea4949336
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-09-12 19:20:31 +0300

    Implemented BinarySerach generic method with examples

---
+++ M08-Generics-and-Collections/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using AlgorithmsAndCollections;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> collection = new List<int>() { 4, 1, 5, 9, 3 };
+            List<string> collection2 = new List<string>() { "hey", "hi", "hello", "Greetings" };
+            collection.Sort();
+            collection2.Sort();
+
+            var target = 3;
+            var target2 = "hi";
+
+            int result = SearchMethods.BinarySerach(collection, 0, collection.Count, target);
+            int result2 = SearchMethods.BinarySerach(collection2, 0, collection2.Count, target2);
+
+            Console.WriteLine($"Index of element {target} was: {result}");
+            Console.WriteLine($"Index of element {target2} was: {result2}");
+        }
+    }
+}
