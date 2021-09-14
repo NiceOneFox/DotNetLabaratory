2287f29778c148b218c5be883f6c322a8886fbe7
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-09-14 15:04:48 +0300

    Implement RPN class

--- M08-Generics-and-Collections/ConsoleApp/Program.cs
+++ M08-Generics-and-Collections/ConsoleApp/Program.cs
@@ -1,51 +1,61 @@
 ﻿using AlgorithmsAndCollections;
+using AlgorithmsAndCollections.Algorithms;
 using System;
 using System.Collections.Generic;
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            List<int> collection = new List<int>() { 4, 1, 5, 9, 3 };
-            List<string> collection2 = new List<string>() { "hey", "hi", "hello", "Greetings" };
-
-            collection.Sort(); // collections must be sorted
-            collection2.Sort();
-
-            var target = 3;
-            var target2 = "hi";
-
-            int result = SearchMethods.BinarySerach(collection, 0, collection.Count, target);
-            int result2 = SearchMethods.BinarySerach(collection2, 0, collection2.Count, target2);
-
-            Console.WriteLine($"Index of element {target} was: {result}");
-            Console.WriteLine($"Index of element {target2} was: {result2}");
-            ///////////////
-
-            MathMethods mathMethods = new MathMethods();
-
-            foreach (var number in mathMethods.Fibonacci())
+            try
             {
-                Console.WriteLine(number);
-            }
-            /////////////
-       
-            AlgorithmsAndCollections.Collections.Queue<int> queue = new AlgorithmsAndCollections.Collections.Queue<int>();
-            queue.Enqueue(5);
-            queue.Enqueue(7);
-            queue.Enqueue(8);
-            queue.Enqueue(2);
+                List<int> collection = new List<int>() { 4, 1, 5, 9, 3 };
+                List<string> collection2 = new List<string>() { "hey", "hi", "hello", "Greetings" };
 
-            Console.WriteLine(queue.Dequeue());
+                collection.Sort(); // collections must be sorted
+                collection2.Sort();
 
-            foreach (int value in queue)
+                var target = 3;
+                var target2 = "hi";
+
+                int result = SearchMethods.BinarySerach(collection, 0, collection.Count, target);
+                int result2 = SearchMethods.BinarySerach(collection2, 0, collection2.Count, target2);
+
+                Console.WriteLine($"Index of element {target} was: {result}");
+                Console.WriteLine($"Index of element {target2} was: {result2}");
+                ///////////////
+
+                MathMethods mathMethods = new MathMethods();
+
+                foreach (var number in mathMethods.Fibonacci())
+                {
+                    Console.WriteLine(number);
+                }
+                /////////////
+
+                AlgorithmsAndCollections.Collections.Queue<int> queue = new AlgorithmsAndCollections.Collections.Queue<int>();
+                queue.Enqueue(5);
+                queue.Enqueue(7);
+                queue.Enqueue(8);
+                queue.Enqueue(2);
+
+                Console.WriteLine(queue.Dequeue());
+
+                foreach (int value in queue)
+                {
+                    Console.Write(value + " ");
+                }
+                ////////////////////
+
+                Console.WriteLine("\nCalculator program");
+                Console.WriteLine(RPN.Calculate("(5,4 + 4) * (3 / 5) - 40"));
+            } 
+            catch (Exception ex)
             {
-                Console.Write(value + " ");
+                Console.WriteLine(ex.Message);
             }
-            ////////////////////
-
         }
     }
 }
