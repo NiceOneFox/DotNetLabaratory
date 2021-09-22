d8e07812684ea8f8bcee61164f27aaae91c072de
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-09-22 20:44:04 +0300

    First init. Class Student. JsonFile

---
+++ M09-Introduction-to-Language-Integrated-Query-(LINQ)/LinqConsole/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqConsole.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string TestName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Assesment { get; set; }
+
+        public Student(string name, string testName, DateTime date, int assesment)
+        {
+            this.Name = name;
+            this.TestName = testName;
+            this.Date = date;
+            this.Assesment = assesment;
+        }
+    }
+}
---
+++ M09-Introduction-to-Language-Integrated-Query-(LINQ)/LinqConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using LinqConsole.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace LinqConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student("Ivan Petrov", "Maths", DateTime.Parse("25/11/2012"),  4);
+
+            List<Student> allStudents = new List<Student>();
+            allStudents.Add(student);
+            allStudents.Add(new Student())
+
+            var jsonString = JsonSerializer.Serialize(allStudents);
+
+            File.WriteAllText(
+                @"C:\Users\Eugene\Documents\GitHub\EpamLaboratoryProjects\M01-Introduction-To-The-Language\M09-Introduction-to-Language-Integrated-Query-(LINQ)\LinqConsole\Data\StudentsData.json", jsonString);
+        }
+    }
+}
