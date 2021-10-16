9cd18b4508e63fd4340aba87362fbcefbf4fb9e8
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-10-16 23:09:41 +0300

    Add Validation for model api

---
+++ Module-10/WebApplicationStudents/WebApplicationStudents/Validation/LectureValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using WebApplicationStudents.Models;
+
+namespace WebApplicationStudents.Validation
+{
+    public class LectureValidation : AbstractValidator<Lecture>
+    {
+        public LectureValidation()
+        {
+            RuleFor(x => x.Date).NotEmpty().GreaterThan(DateTime.Now).WithMessage("Date of lecture must be in future");
+            RuleFor(x => x.Name).Length(2, 120);
+        }
+
+    }
+}
---
+++ Module-10/WebApplicationStudents/WebApplicationStudents/Validation/MarkValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using WebApplicationStudents.Models;
+
+namespace WebApplicationStudents.Validation
+{
+    public class MarkValidation : AbstractValidator<Mark>
+    {
+        public MarkValidation()
+        {
+            RuleFor(x => x.Assesment).NotEmpty().NotNull().GreaterThan(-1).LessThan(5).
+                WithMessage("Assesment value must be between 0 and 5");
+            RuleFor(x => x.Text).MaximumLength(255);
+        }
+    }
+}
