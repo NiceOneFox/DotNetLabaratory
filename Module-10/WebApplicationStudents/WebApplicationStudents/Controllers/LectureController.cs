007dfd614bee9aad8f4a48ced4aff27d4a901c5d
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-10-20 02:28:40 +0300

    Add Lecture Controller

---
+++ Module-10/WebApplicationStudents/WebApplicationStudents/Controllers/LectureController.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLogic.Models;
+using BusinessLogic.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationStudents.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class LectureController : ControllerBase
+    {
+        private readonly ILectureService _lectureService;
+
+        private readonly ILogger _logger;
+
+        public LectureController(ILectureService lectureService, ILogger logger)
+        {
+            _lectureService = lectureService;
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")] 
+        public ActionResult<Lecture> GetLecture(int id)
+        {
+            return _lectureService.Get(id) switch
+            {
+                null => NotFound(),
+                var lector => lector // implicit cast to AcitonResult
+            };
+        }
+
+        [HttpGet]
+        public ActionResult<IReadOnlyCollection<Lector>> GetLectures()
+        {
+            return _lectureService.GetAll().ToArray();
+        }
+
+        [HttpPost]
+        public ActionResult AddLecture(Lecture lecture)
+        {
+            var newLectureId = _lectureService.New(lecture);
+            return Ok($"api/lecture/{newLectureId}");
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<string> UpdateLecture(int id, Lecture lecture)
+        {
+            var lectureId = _lectureService.Edit(lecture with { Id = id });
+            return Ok($"api/lecture/{lectureId}");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteLecture(int id)
+        {
+            _lectureService.Delete(id);
+            return Ok();
+        }
+
+    }
+}
