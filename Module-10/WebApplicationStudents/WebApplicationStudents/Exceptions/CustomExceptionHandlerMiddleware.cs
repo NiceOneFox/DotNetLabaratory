55ba1ac48b49de938e011fd54bd631924fef981d
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-10-21 01:25:52 +0300

    Add Custom ExceptioHandler

---
+++ Module-10/WebApplicationStudents/WebApplicationStudents/Exceptions/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text.Json;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplicationStudents.Exceptions
+{
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next) =>
+            _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var statusCode = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+
+            switch (ex)
+            {
+                case ValidationException validationException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Errors);
+                    break;
+
+                default:
+                    statusCode = HttpStatusCode.NotFound;
+                    break;
+
+            }
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { error = ex.Message });
+            }
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
