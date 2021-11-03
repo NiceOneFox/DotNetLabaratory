d3ba228c5b8f0c5178c1bffdf760ecf633d84b19
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-11-04 01:14:32 +0300

    StudentService Delete method Test

--- Module-10/WebApplicationStudents/WebApplicationStudentsTests/BusinessLogicTests/StudentServiceTests.cs
+++ Module-10/WebApplicationStudents/WebApplicationStudentsTests/BusinessLogicTests/StudentServiceTests.cs
@@ -3,20 +3,68 @@ using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
 using Moq;
+using DatabaseAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+using DatabaseAccess;
+using Microsoft.EntityFrameworkCore.InMemory;
+using System.Linq;
+using DatabaseAccess.Configuration;
+using AutoMapper;
+using BusinessLogic.Models;
+using DatabaseAccess.Models;
+using BusinessLogic.Services;
+using BusinessLogic.Mappers;
 
 namespace WebApplicationStudentsTests.BusinessLogicTests
 {
     class StudentServiceTests
     {
+        private CourseDbContext _context { get; set; }
         [SetUp]
         public void Setup()
         {
+            var options = new DbContextOptionsBuilder<CourseDbContext>()
+                .UseInMemoryDatabase(databaseName: "CourseDb")
+                .Options;
+
+            _context = new CourseDbContext(options);
+            
+            _context.Database.EnsureCreated();
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
         }
 
         [Test]
-        public void Test1()
+        public void Delete_ExistingId_ReturnsStudent()
         {
-            Assert.Pass();
+            //arrange
+            
+            int expectedCountStudents = _context.Students.Count() - 1;
+            int id = 1;
+
+            IStudentRepository studentRepository = new StudentRepository(_context);
+           
+            var myProfile = new MapperBl();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            IMapper mapper = new Mapper(configuration);
+            
+            var studentService = new StudentService(studentRepository, mapper);
+            // act
+            studentService.Delete(id);
+            int countStudent = _context.Students.Count();
+
+            //assert
+            Assert.AreEqual(expectedCountStudents, countStudent);
+        }
+
+        public void Delete_UnexistingId_ThrowsException()
+        {
+
         }
     }
 }
